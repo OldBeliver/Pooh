e7ef938d4c592aad64a81ecee8b0a28c3c4d9d9c
OldBeliver <380506338004@yandex.ua>
2021-12-19 20:50:59 +0200

    исправления по замечаниям Ментора
    2. имя переменной canSpawn приведено в соответствие _canSpawn
    3. Корутина SpawnBee() зациклена в Start
    
    1. _direction = Vector3.left if (transform.position.x < 0) _direction = Vector3.right - не смог сократить до одной строчки

--- Assets/Scripts/Bee/BeeSprite.cs
+++ Assets/Scripts/Bee/BeeSprite.cs
@@ -16,20 +16,10 @@ public class BeeSprite : MonoBehaviour
         int randomIndex = GetSpriteIndex(_sprites.Length);
 
         _spriteRenderer.sprite = _sprites[randomIndex];
-
-        TryFlipSprite();
     }
 
     private int GetSpriteIndex(int spritesCount)
     {
         return Random.Range(0, spritesCount);
-    }
-
-    private void TryFlipSprite()
-    {
-        _spriteRenderer.flipX = false;
-
-        if (transform.position.x < 0)
-            _spriteRenderer.flipX = true;
     }
 }
--- Assets/Scripts/BeeSpawner/Spawner.cs
+++ Assets/Scripts/BeeSpawner/Spawner.cs
@@ -9,39 +9,34 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Transform[] _spawnPointers;
     [SerializeField] private GameObject _container;
     [SerializeField] private int _quantity;
-
-    private bool canSpawn = true;
+    
     private List<GameObject> _pool = new List<GameObject>();
-
+    
     private void Start()
     {
         LoadObjectPool(_quantity);
-    }
 
-    private void Update()
-    {
-        if (canSpawn)
-            StartCoroutine(SpawnBee());
+        StartCoroutine(SpawnBee());
     }
 
     private IEnumerator SpawnBee()
-    {
-        canSpawn = false;
-
+    {   
         int randomPoint = Random.Range(0, _spawnPointers.Length);
-        int randomBee = Random.Range(0, _pool.Count);
 
         Transform coordinate = _spawnPointers[randomPoint];
-        
+
         if (TryGetBee(out GameObject bee))
         {
             bee.transform.position = coordinate.position;
+
+            TryFlipSprite(bee);
+
             bee.SetActive(true);
 
             yield return new WaitForSeconds(2f);
         }
 
-        canSpawn = true;
+        StartCoroutine(SpawnBee());
     }
 
     private void LoadObjectPool(int count)
@@ -49,6 +44,7 @@ public class Spawner : MonoBehaviour
         for (int i = 0; i < count; i++)
         {
             GameObject spawned = Instantiate(_subject, _container.transform);
+            
             spawned.SetActive(false);
 
             _pool.Add(spawned);
@@ -57,8 +53,17 @@ public class Spawner : MonoBehaviour
 
     private bool TryGetBee(out GameObject result)
     {
-        result = _pool.First(p => p.activeSelf == false);
+        result = _pool.First(subject => subject.activeSelf == false);
 
         return result != null;
+    }
+
+    private void TryFlipSprite(GameObject subject)
+    {
+        SpriteRenderer sprite = subject.GetComponent<SpriteRenderer>();
+        sprite.flipX = false;
+
+        if (subject.transform.position.x < 0)
+            sprite.flipX = true;
     }
 }
