a7de4b2104eff85522dbe9971fe7686136eca859
OldBeliver <380506338004@yandex.ua>
2022-05-24 22:42:42 +0300

    finish script

--- Assets/Scripts/BeeSpawner/ObjectPool.cs
+++ Assets/Scripts/BeeSpawner/ObjectPool.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    [SerializeField] private GameObject _container;
-    [SerializeField] private int _quantity;
+    [SerializeField] private GameObject _container;    
+
+    private List<GameObject> _pool = new List<GameObject>();
+
+    protected void Initialize(GameObject prefab)
+    {
+        GameObject spawned = Instantiate(prefab, _container.transform);
+        
+        spawned.SetActive(false);
+
+        _pool.Add(spawned);
+    }
+
+    protected bool TryGetBee(out GameObject result)
+    {
+        result = _pool.First(bee => bee.activeSelf == false);
+
+        return result != null;
+    }
 }
--- Assets/Scripts/BeeSpawner/Spawnist.cs
+++ Assets/Scripts/BeeSpawner/Spawnist.cs
@@ -4,5 +4,51 @@ using UnityEngine;
 
 public class Spawnist : ObjectPool
 {
-    
+    [SerializeField] private GameObject _bee;
+    [SerializeField] private Transform[] _spawnPoints;    
+    [SerializeField] private int _quantity;
+
+    private void Start()
+    {
+        CreateBees(_quantity);
+
+        StartCoroutine(SpawnBee());
+    }
+
+    private IEnumerator SpawnBee()
+    {
+        int randomPoint = Random.Range(0, _spawnPoints.Length);
+
+        Transform coordinate = _spawnPoints[randomPoint];
+
+        if (TryGetBee(out GameObject bee))
+        {
+            bee.transform.position = coordinate.position;
+
+            TryFlipSprite(bee);
+
+            bee.SetActive(true);
+
+            yield return new WaitForSeconds(2f);
+        }
+
+        StartCoroutine(SpawnBee());
+    }
+
+    private void TryFlipSprite(GameObject subject)
+    {
+        SpriteRenderer sprite = subject.GetComponent<SpriteRenderer>();
+        sprite.flipX = false;
+
+        if (subject.transform.position.x < 0)
+            sprite.flipX = true;
+    }
+
+    private void CreateBees(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Initialize(_bee);
+        }
+    }
 }
